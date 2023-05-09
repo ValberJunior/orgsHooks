db1aa0c16f9375cbbd461bf4c772064fd92a8972
ValberJunior <valberjr1992@gmail.com>
2023-05-08 21:35:02 -0300

    ✨feature: add useMemo

--- src/screens/home/producersSection/producerCard/index.js
+++ src/screens/home/producersSection/producerCard/index.js
@@ -1,7 +1,11 @@
-import React, { useReducer } from 'react';
+import React, { useMemo, useReducer } from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import { StarsComponent } from '../../../../components';
+
+const addMeasure= ( distance ) => {
+  return `${distance}m`
+}
 
 const ProducerCard = ({name, image, distance, stars}) => {
   
@@ -9,6 +13,10 @@ const ProducerCard = ({name, image, distance, stars}) => {
   const [selected, reverseSelected] = useReducer(
     (selected)=> !selected , false
     );
+  // Adicionando o useMemo
+  const measuredDistance = useMemo(
+    ()=> addMeasure(distance)
+    ,[distance]);
 
   return (
     <TouchableOpacity style={styles.card} onPress={reverseSelected}>
@@ -18,7 +26,7 @@ const ProducerCard = ({name, image, distance, stars}) => {
                 <Text style={styles.name}>{ name }</Text>
                 <StarsComponent feedback={stars} editable={selected} big={selected} />
             </View>
-            <Text style={styles.distance}>{ distance }</Text>
+            <Text style={styles.distance}>{ measuredDistance }</Text>
         </View>     
     </TouchableOpacity>
   )
